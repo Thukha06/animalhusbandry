b11ab29eb70fcb75afae32deef86e9145da09cf7
Myat Thu Kha <135417060+Thukha06@users.noreply.github.com>
2024-08-20 13:32:17 +0630

    amended to also return price, not only unit

--- admin/dist/php/get_unit.php
+++ admin/dist/php/get_unit.php
@@ -4,13 +4,14 @@ include('database.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $product_id = $_POST['product_id'];
 
-    $stmt = $db->prepare("SELECT product_unit FROM product_type WHERE product_id = :product_id");
+    $stmt = $db->prepare("SELECT product_price, product_unit FROM product_type WHERE product_id = :product_id");
     $stmt->bindParam(':product_id', $product_id, PDO::PARAM_INT);
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    $product_price = $result ? $result['product_price'] : '';
     $product_unit = $result ? $result['product_unit'] : '';
 
-    echo json_encode(['unit' => $product_unit]);
+    echo json_encode(['unit' => $product_unit, 'price' => $product_price]);
 }
 ?>
